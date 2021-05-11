55318310b55ac6bed108351a63e38bcfe9763937
Maia <maia.mcc@gmail.com>
2021-05-11 12:29:01 -0400

    provide instructions for getting kubernetes client generator if it doesn't exist

--- genapi.sh
+++ genapi.sh
@@ -4,6 +4,14 @@ set -ex
 sed 's/com.github.tilt-dev.tilt.pkg.apis.core.//g' src/gen/swagger.json.original > src/gen/swagger.json.unprocessed
 
 # Run the Kubernetes codegen
+genpath="../gen"
+if [ ! -d "$genpath" ]; then
+  echo "Kubernetes client generator not found at $genpath"
+  echo "In the directory above this one, run:"
+  echo "\tgit clone https://github.com/kubernetes-client/gen.git"
+  exit 1
+fi
+
 ../gen/openapi/typescript.sh src/gen settings
 
 # Make all the imports consistent with how our typescript/nextjs config
